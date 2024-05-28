7a49be853077416803be3cf4ef3ae487fc5f68a6
JunaidARahat <mjunaidrahat1990@gmail.com>
2024-05-28 11:13:56 +0500

    code updated

--- demo.py
+++ demo.py
@@ -1,4 +1,6 @@
 from heart_stroke.pipline.pipeline import Pipeline
+from heart_stroke.config.configuration import Configuartion
+from heart_stroke.constants import get_current_time_stamp
 
-pipline=Pipeline()
-pipline.run_pipelines()
+pipline=Pipeline(config=Configuartion(current_time_stamp=get_current_time_stamp()))
+pipline.run_pipeline()
